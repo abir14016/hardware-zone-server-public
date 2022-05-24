3072d0b89e6d70f832e312552c8058f87244d828
abir14016 <abir14016@gmail.com>
2022-05-25 00:56:05 +0600

    tools loaded from mongodb

--- index.js
+++ index.js
@@ -14,11 +14,19 @@ app.use(express.json());
 //connect with mongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ecura.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri);
 
 async function run() {
     try {
+        await client.connect();
+        const toolCollection = client.db("hardwareZone").collection("tool");
 
+        // load all tools from database
+        app.get('/tool', async (req, res) => {
+            const query = {};
+            const cursor = toolCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        });
     }
     finally {
         //comment
