966311d5f943e6ed42f820adfef53267a76bedb6
abir14016 <abir14016@gmail.com>
2022-05-25 00:16:07 +0600

    server connected with mongoDB

--- index.js
+++ index.js
@@ -2,12 +2,29 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//connect with mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ecura.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
+
+async function run() {
+    try {
+
+    }
+    finally {
+        //comment
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hardware zone server running');
